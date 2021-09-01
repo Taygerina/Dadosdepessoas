c1817f0bacdb81234461b11264e107210adee55d
Talita <talitamarcelo15@gmail.com>
2021-09-01 20:26:06 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dadosdepessoas
+{
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            //Declaraçao da variavel da classe triangulo x
+            usuario x;
+
+            //Declaraçao da variavel da classe triangulo y
+            usuario y;
+
+            //instaciar a variavel
+            x = new usuario();
+            //instaciar a variavel
+            y = new usuario();
+
+
+            //informaçao para o meu usuario
+            Console.WriteLine("Qual é o nome do Usuário?");
+            x.nome = Console.ReadLine();
+            Console.WriteLine("Qual é a idade do usuário?");
+            x.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+           
+
+
+            //informaçao para o meu usuario
+            Console.WriteLine("Qual é o nome do segundo usuário?");
+            y.nome = Console.ReadLine();
+            Console.WriteLine("Qual é a idade do segundo usuário?");
+            y.idade = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            
+
+            
+
+
+            //definição da area maior
+            if (y.idade > x.idade)
+            {
+                Console.WriteLine(y.nome + " é a pessoa mais velha!!");
+            }
+            else
+            {
+                Console.WriteLine(x.nome + " é a pessoa mais velha!!");
+            }
+
+            Console.ReadKey();
+           
+        }
+    }
+}
---
+++ usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dadosdepessoas
+{
+  class usuario
+    { 
+        //declarando variavel do nome 
+       public string nome;
+        //declarando uma variavel da idade
+       public int idade;
+        
+    }
+}
